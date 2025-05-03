11d25d4cf75ee35a8f0f3007c77d609db7696658
Harshit Batra <hb99960@gmail.com>
2025-05-03 18:41:41 +0530

    Add files via upload

---
+++ S01/index.ts
@@ -0,0 +1,35 @@
+console.log("My first TypeScript code!");
+console.log("Hello, world!");
+let s = "Hello, world!";
+let str:string = "Hello world!";
+console.log(s);
+console.log(str);
+
+/** Types 
+ * 
+ * string
+ * number
+ * boolean
+*/
+let a: string = "Hello world!";
+let b: number = 42;
+let c: boolean = true;  
+
+console.log(a);
+console.log(b);
+console.log(c);
+let d:object = {id:1, subject:"NSD"};
+console.log(d);
+// array
+let e:number[] = [1,2,3,4,5];
+console.log(e);
+
+/**
+ * Important from interview pov
+ * any, null, undefined, void
+ */
+
+let f = [10,11,12];
+let g = [10,11,12, "Hello"];
+console.log(f);
+
